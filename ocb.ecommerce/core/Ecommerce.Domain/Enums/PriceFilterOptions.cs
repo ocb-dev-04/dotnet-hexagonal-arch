62421ed68a5b9648636ff6ab9f76437e9c6abb24
Oscar Chavez - Terabit Lab <oscarchb04@gmail.com>
2022-10-07 04:40:18 -0400

    *More summary removed

--- ocb.ecommerce/core/Ecommerce.Domain/Enums/PriceFilterOptions.cs
+++ ocb.ecommerce/core/Ecommerce.Domain/Enums/PriceFilterOptions.cs
@@ -2,13 +2,10 @@
 
 namespace Ecommerce.Domain.Enums;
 
-/// <summary>
-/// Enums to filter <see cref="Product"/> by price
-/// </summary>
 public enum PriceFilterOptions
 {
     Equal = 0,
     NotEqual,
     GreaterThan,
-    LessThan,
+    LessThan
 }
--- ocb.ecommerce/core/Ecommerce.Domain/Extensions/QueryableExtensions.cs
+++ ocb.ecommerce/core/Ecommerce.Domain/Extensions/QueryableExtensions.cs
@@ -3,18 +3,8 @@ using Ecommerce.Domain.Entities;
 
 namespace Ecommerce.Domain.Extensions;
 
-/// <summary>
-/// <see cref="IQueryable"/> Extensions methods to make more easy filter and search. Here there are only Repository extension methods.
-/// </summary>
 public static class QueryableExtensions
 {
-    /// <summary>
-    /// Filter <see cref="Product"/> by pricc
-    /// </summary>
-    /// <param name="query"></param>
-    /// <param name="price"></param>
-    /// <param name="filterOptions"></param>
-    /// <returns></returns>
     public static IQueryable<Product> FilterByPrice(this IQueryable<Product> query, double price, PriceFilterOptions filterOptions)
         => filterOptions switch
         {
